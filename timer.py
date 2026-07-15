afdc15d17da4c64204dbe98c05c05680df585589
mamoona fayyaz <mamoonafayyaz92@gmail.com>
2026-07-15 12:49:34 +0500

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,12 @@
+import time
+
+startnumber = 5
+
+print("Get ready...")
+time.sleep(1)
+
+for i in range(startnumber, 0, -1):
+    print(f"{i}...")
+    time.sleep(1)
+
+print("\n HAPPY BIRTHDAY ")
